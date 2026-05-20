96ebe73efd2f93eea35b31a0da3178f542f39e19
nishat-supty244 <imsupty@gmail.com>
2026-05-20 13:25:58 +0200

    Delete cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-rm -f cracked.pot 4-password.txt
-john --format=raw-md5 --wordlist=/usr/share/wordlists/rockyou.txt "$1"
-john --format=raw-md5 --show "$1" | cut -d: -f2 | grep -v '^$' > 4-password.txt
