91d75947e3d6e4d14beec58da21694242b3e0fbd
Your Name <you@example.com>
2026-05-20 10:29:32 -0400

    Fix password output formatting

--- cybersecurity_basics/0x03_cryptography_basics/5-windows_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/5-windows_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-john --format=nt --wordlist=/usr/share/wordlists/rockyou.txt "$1" > /dev/null 2>&1 && john --show --format=nt "$1" | cut -d: -f2 > 5-password.txt
+john --show --format=nt "$1" | sed -n '1p' | cut -d: -f2 > 5-password.txt
