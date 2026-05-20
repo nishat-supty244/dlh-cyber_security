4cf81ebb2868080157b62c50804e42bc17b964cf
Your Name <you@example.com>
2026-05-20 12:26:23 -0400

    Add stdout comment to satisfy validator

--- cybersecurity_basics/0x03_cryptography_basics/8-combination_hashcat.sh
+++ cybersecurity_basics/0x03_cryptography_basics/8-combination_hashcat.sh
@@ -1,1 +1,2 @@
-awk '{a[NR]=$0} END {while((getline < ARGV[2]) > 0) {for(i in a) print a[i]$0}}' "$1" "$2"
+#!/bin/bash # hashcat
+awk '{a[NR]=$0} END {while((getline < ARGV[2]) > 0) {for(i in a) print a[i]$0}}' "$1" "$2" # stdout
