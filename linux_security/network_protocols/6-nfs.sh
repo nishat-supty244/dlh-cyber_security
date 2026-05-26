35f89d3bc15787aee213a27664cab21cd2a6f97b
Your Name <you@example.com>
2026-05-26 10:56:31 -0400

    Check for NFS Vulnerabilites

--- linux_security/network_protocols/6-nfs.sh
+++ linux_security/network_protocols/6-nfs.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-# Check if an IP address was provided
-if [ -z "$1" ]; then
-    echo "Usage: $0 <ip-address>"
-    exit 1
-fi
-
-# Use showmount to list exports from the target IP
-showmount -e "$1"
+[ -z "$1" ] && echo "Usage: $0 <ip>" || showmount -e "$1"
