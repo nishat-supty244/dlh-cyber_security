2cd19d77402b44a0c5035e574cf386871ac86bc6
Your Name <you@example.com>
2026-05-26 13:06:11 -0400

    Who is it

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
-
-# 1. We run 'whois' on the domain provided ($1)
-# 2. We send (|) that messy info into 'awk' to clean it up
-whois "$1" | awk -F': ' ' 
-
-# The -F': ' tells awk: "When you see a colon and a space, that is where you split the line!"
-
-/^Registrant Name:/ { print "Registrant Name," $2 }
-/^Registrant Organization:/ { print "Registrant Organization," $2 }
-/^Registrant Street:/ { print "Registrant Street," $2 " " }
-/^Registrant City:/ { print "Registrant City," $2 }
-/^Registrant Postal Code:/ { print "Registrant Postal Code," $2 }
- "$1.csv"
+whois "$1" | awk -F': +' '
+$1 == "Registrant Name" { print "Registrant Name," $2 }
+$1 == "Admin Name" { print "Admin Name," $2 }
+' > "$1.csv"
