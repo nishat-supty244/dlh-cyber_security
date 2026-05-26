4f67a524a933615a6268b7358225f1a7d8171aaf
Your Name <you@example.com>
2026-05-26 10:21:19 -0400

    Audit SSH Config

--- linux_security/network_protocols/4-audit.sh
+++ linux_security/network_protocols/4-audit.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep -E "PermitRootLogin yes|PasswordAuthentication yes|X11Forwarding yes" /etc/ssh/sshd_config /etc/ssh/sshd_config.d/*.conf 2>/dev/null
+grep -Ev "PermitRootLogin no|PasswordAuthentication no|X11Forwarding no" /etc/ssh/sshd_config /etc/ssh/sshd_config.d/*.conf 2>/dev/null
