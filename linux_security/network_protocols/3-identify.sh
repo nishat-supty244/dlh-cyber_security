8c84ed45a23bef6552498832cb8ae17d3d92a65d
Your Name <you@example.com>
2026-05-26 09:55:42 -0400

    Identifying common vulnerabilities

--- linux_security/network_protocols/3-identify.sh
+++ linux_security/network_protocols/3-identify.sh
@@ -1,8 +1,2 @@
-
 #!/bin/bash
-if command -v lynis >/dev/null 2>&1; then
-    sudo lynis audit system --quick
-else
-    echo "Lynis is not installed."
-    exit 1
-fi
+command -v lynis >/dev/null 2>&1 && sudo lynis audit system --quick || echo "Lynis not installed"
