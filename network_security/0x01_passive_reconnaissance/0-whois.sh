d313a6a4aea3572b76b763ef76b0a6764172932f
Your Name <you@example.com>
2026-05-26 13:08:28 -0400

    Who is it

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois "$1" | grep -E "^Registrant Name:|^Admin Name:" | awk -F': +' '{print $1 "," $2}' > "$1.csv"
+whois "$1" | grep -E "^Registrant Name:|^Admin Name:|^Tech Name:" | awk -F': +' '{print $1 "," $2}' > "$1.csv"
