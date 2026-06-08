25c64839d891573b1777e1946ca9bab272816498
Your Name <you@example.com>
2026-06-08 15:24:07 -0400

    (A2:2021) - Cryptographic Failures - Scripting completed

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -1,2 +1,8 @@
-#!/usr/bin/python3
-import sys, base64; print("".join([chr(b ^ 95) for b in base64.b64decode(sys.argv[1].replace("{xor}", ""))]))
+#!/bin/bash
+
+# Remove the {xor} prefix using parameter expansion
+val=${1#"{xor}"}
+
+# Use echo and perl to perform the XOR operation
+# This avoids shell metacharacters and complex block structures
+echo "$val" | base64 -d | perl -ne 'BEGIN {$k=0x5F} print chr(ord($_) ^ $k) for split ""'
